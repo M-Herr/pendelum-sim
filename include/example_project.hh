f2dcf6020340be41924bb6d83936315a123d347d
Mark <m.ryherr@pm.me>
2024-05-27 21:43:19 -0500

    Added spdlog as dependency that's actually used in this example

--- include/example_project.hh
+++ include/example_project.hh
@@ -2,9 +2,27 @@
 #ifndef __EXAMPLE_PROJECT_HH__
 #define __EXAMPLE_PROJECT_HH__
 
-struct TestStruct 
-{
+#include <memory>
 
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
+class Logger {
+    public:
+    static std::shared_ptr<spdlog::logger> getInstance() {
+        static std::shared_ptr<spdlog::logger> instance = spdlog::basic_logger_mt("basic_logger", "logs/log.log");
+        return instance;
+    }
+
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+
+    private:
+    Logger() {
+        spdlog::set_level(spdlog::level::debug);
+        spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%l] [%n] %v"); // Set log pattern
+    }
 };
+
 
 #endif

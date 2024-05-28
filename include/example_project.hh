157b56e6b653f2d271f89ae324f0e0d8e287b042
Mark <m.ryherr@pm.me>
2024-05-28 00:11:33 -0500

    Created two loggers.  Added simple signal handler

--- include/example_project.hh
+++ include/example_project.hh
@@ -6,19 +6,40 @@
 
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <vector>
 
-class Logger {
+
+class ConsoleLogger 
+{
     public:
     static std::shared_ptr<spdlog::logger> getInstance() {
-        static std::shared_ptr<spdlog::logger> instance = spdlog::basic_logger_mt("basic_logger", "logs/log.log");
+            static std::shared_ptr<spdlog::logger> instance = spdlog::stdout_color_mt("console_logger");
+            return instance;
+        }
+
+    ConsoleLogger(const ConsoleLogger&) = delete;
+    ConsoleLogger& operator=(const ConsoleLogger&) = delete;
+    private:
+    ConsoleLogger() {
+        spdlog::set_level(spdlog::level::debug);
+        spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%l] [%n] %v"); // Set log pattern
+    }
+};
+
+class FileLogger {
+    public:
+
+    static std::shared_ptr<spdlog::logger> getInstance() {
+        static std::shared_ptr<spdlog::logger> instance = spdlog::basic_logger_mt("file_logger", "logs/log.log");
         return instance;
     }
-
-    Logger(const Logger&) = delete;
-    Logger& operator=(const Logger&) = delete;
+    
+    FileLogger(const FileLogger&) = delete;
+    FileLogger& operator=(const FileLogger&) = delete;
 
     private:
-    Logger() {
+    FileLogger() {
         spdlog::set_level(spdlog::level::debug);
         spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%l] [%n] %v"); // Set log pattern
     }
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 #include <string>
+#include <csignal>
+#include <chrono>
+
 
 #include "example_project.hh"
-int main() {
 
-    std::cout << "Hello World!" << std::endl;
+
+std::atomic<bool> running{true};
+
+
+void signalHandler(int signal) {
+    if(signal == SIGINT) {
+        running = false;
+        auto console_logger = ConsoleLogger::getInstance();
+        console_logger->warn("Got a SIGINT, shutting down");
+    }
+}
+
+int main() 
+{
+    std::signal(SIGINT, signalHandler);
+
+    std::cout << "Hello world!  Here's a simple CMake Example with two dependencies.  One for testing, one for runtime." << std::endl;
+
+    auto file_logger = FileLogger::getInstance();
+    auto console_logger = ConsoleLogger::getInstance();
+
+    console_logger->info("Starting the loop!");
+
+    while(running) {
+
+        console_logger->info("Running...");
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    } 
+
+
+
     return EXIT_SUCCESS;
 }
 
